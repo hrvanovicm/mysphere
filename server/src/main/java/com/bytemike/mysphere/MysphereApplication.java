bcbbc501ed9cfa036ff2255cd55c430a992df442
bytemike <hrvanovicmirza64@gmail.com>
2024-03-19 23:30:45 +0100

    spring server init

---
+++ server/src/main/java/com/bytemike/mysphere/MysphereApplication.java
@@ -0,0 +1,13 @@
+package com.bytemike.mysphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysphereApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/bytemike/mysphere/MysphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytemike.mysphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
